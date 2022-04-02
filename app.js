e0751b37b826e2cdcfa0fdaf61a8a7c029284e4b
valerieroman <81587687+valerieroman@users.noreply.github.com>
2022-04-02 19:59:35 -0400

    Added modal classes, and event listener to open and close modal window

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // select modal-btn,modal-overlay,close-btn
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+modalBtn.addEventListener("click", function () {
+modal.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+modal.classList.remove("open-modal");
+});
+
